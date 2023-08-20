653d8c898529c1564e2093f1b833a633690bcc2f
Süheyla Sarıkaya <suheyla.sarikaya@tubitak.gov.tr>
2023-08-20 10:16:49 +0300

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'httpss://app-vending-machine-1-4d0474825892.herokuapp.com',
+      target: 'https://app-vending-machine-1-4d0474825892.herokuapp.com',
       changeOrigin: true,
     })
   );
